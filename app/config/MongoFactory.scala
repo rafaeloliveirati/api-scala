ede025efa4e310c72d36dc2c55663bcb67346482
Rafael Oliveira <rafaeloliveira.ti@live.com>
2018-07-30 22:07:50 -0300

    add mongo config to application.conf

--- app/config/MongoFactory.scala
+++ app/config/MongoFactory.scala
@@ -1,14 +1,16 @@
 package config
 
 import com.mongodb.casbah.MongoConnection
+import com.typesafe.config.ConfigFactory
 
 object MongoFactory {
-  private val SERVER = "localhost"
-  private val PORT = 27017
-  private val DATABASE = "scala"
-  private val USER_COLLECTION = "user"
-  private val PURCHASE_COLLECTION = "purchase"
-  private val TEMPLATE_COLLECTION = "template"
+  private val config = ConfigFactory.load()
+  private val SERVER = config.getString("mongo.hostname")
+  private val PORT = config.getString("mongo.port")
+  private val DATABASE = config.getString("mongo.db")
+  private val USER_COLLECTION = config.getString("mongo.collections.user")
+  private val PURCHASE_COLLECTION = config.getString("mongo.collections.purchase")
+  private val TEMPLATE_COLLECTION = config.getString("mongo.collections.template")
   private val connection = MongoConnection(SERVER)
   val userCollection = connection(DATABASE)(USER_COLLECTION)
   val purchaseCollection = connection(DATABASE)(PURCHASE_COLLECTION)
